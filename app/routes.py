286de3534d767dfd5e186cf422f4d38a45ab0710
xinqich <timurcallo@gmail.com>
2025-09-10 22:15:48 +0300

    Add color name processing
    On form submission, the selected color is now converted mapped to a color name using `rgb2text/get_color_name`. The result is passed to the result template.

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,23 @@
+import webcolors
 from flask import render_template, request, redirect, url_for
+from webcolors import hex_to_rgb
+
 from app import app
+from .rgb2text import get_color_name
+
 
 @app.route("/")
 def form():
     return render_template("form.html")
+
 
 @app.route("/submit", methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
-        return render_template("result.html", name=name, email=email)
+        color = request.form.get("color")
+        text_color = get_color_name(hex_to_rgb(color))
+        return render_template("result.html", name=name, email=email, color=text_color)
     else:
         return redirect(url_for("form"))
